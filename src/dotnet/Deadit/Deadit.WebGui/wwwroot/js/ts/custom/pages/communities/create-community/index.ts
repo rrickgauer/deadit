67d660d6e58be0951497c7387cd043c342567d33
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-12-29 22:29:30 -0600

    layout the form inputs

--- src/dotnet/Deadit/Deadit.WebGui/wwwroot/js/ts/custom/pages/communities/create-community/index.ts
+++ src/dotnet/Deadit/Deadit.WebGui/wwwroot/js/ts/custom/pages/communities/create-community/index.ts
@@ -1,12 +1,34 @@
 import { LoginModalController } from "../../../components/login-modal/login-modal-controller";
+import { NativeEvents } from "../../../domain/constants/native-events";
 import { PageUtilities } from "../../../utilities/page-utilities";
 
 
 PageUtilities.pageReady(async () =>
 {
     const loginModal = LoginModalController.setupPage();
-    alert('create community');
+    testing();
+    
 });
+
+
+function testing()
+{
+    /*    document.querySelector()*/
+
+    /*    create-community-form*/
+
+    const form: HTMLFormElement = document.querySelector('.create-community-form');
+
+    form.addEventListener(NativeEvents.Submit, async (e) =>
+    {
+        e.preventDefault();
+
+        if (confirm('Clear form?'))
+        {
+            form.reset();
+        }
+    });
+}
 
 
 
