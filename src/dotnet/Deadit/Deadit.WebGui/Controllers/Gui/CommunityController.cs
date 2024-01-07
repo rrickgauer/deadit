7a64762805a89ce4450b8041529b6310ecd61d37
Ryan Rickgauer <rrickgauer1@gmail.com>
2024-01-07 10:27:48 -0600

    add community controller/view

---
+++ src/dotnet/Deadit/Deadit.WebGui/Controllers/Gui/CommunityController.cs
@@ -0,0 +1,23 @@
+﻿using Deadit.Lib.Domain.Constants;
+using Deadit.WebGui.Controllers.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deadit.WebGui.Controllers.Gui;
+
+[Controller]
+[Route("/c")]
+public class CommunityController : Controller, IControllerName
+{
+    public static string ControllerRedirectName => IControllerName.RemoveControllerSuffix(nameof(CommunityController));
+
+    /// <summary>
+    /// /c/:communityName
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("{communityName}")]
+    [ActionName(nameof(GetCommunityPage))]
+    public async Task<IActionResult> GetCommunityPage([FromRoute] string communityName)
+    {
+        return View(GuiPageViewFiles.CommunityPage);
+    }
+}
---
+++ src/dotnet/Deadit/Deadit.WebGui/wwwroot/js/ts/custom/pages/community/community-page/community-page-controller.ts
@@ -0,0 +1,20 @@
+import { LoginModal } from "../../../components/login-modal/login-modal";
+
+
+
+export class CommunityPageController
+{
+
+    private _modal = new LoginModal(true);
+
+    constructor()
+    {
+        
+    }
+
+    control = () =>
+    {
+        
+    }
+}
+
