76cac0a1b1de425a5cfdbd9404793a6f67b02eb1
Ryan Rickgauer <rrickgauer1@gmail.com>
2024-05-08 10:07:37 -0500

    clean up

--- src/dotnet/Deadit/Deadit.WebGui/wwwroot/js/ts/custom/pages/community/create-post/index.ts
+++ src/dotnet/Deadit/Deadit.WebGui/wwwroot/js/ts/custom/pages/community/create-post/index.ts
@@ -1,7 +1,3 @@
-import { NativeEvents } from "../../../domain/constants/native-events";
-import { MessageBoxConfirm } from "../../../domain/helpers/message-box/MessageBoxConfirm";
-import { MessageBoxUtilities } from "../../../utilities/message-box-utilities";
-import { AlertUtilities } from "../../../utilities/page-alert-utilities";
 import { PageUtilities } from "../../../utilities/page-utilities";
 import { CreatePostController } from "./create-post-controller";
 
@@ -12,57 +8,7 @@ PageUtilities.pageReady(async () =>
 {
     const controller = new CreatePostController();
     controller.control();
-
-
-    testMessageBox();
-
 });
 
 
-
-function testMessageBox()
-{
-
-    document.querySelector('#btn-standard').addEventListener(NativeEvents.Click, (e) =>
-    {
-        AlertUtilities.showSuccess({
-            container: document.querySelector('#test-alerts-container'),
-            message: 'This is success',
-        });
-    });
-
-
-    document.querySelector('#btn-error').addEventListener(NativeEvents.Click, (e) =>
-    {
-        AlertUtilities.showDanger({
-            container: document.querySelector('#test-alerts-container'),
-            message: 'This is danger',
-        });
-    });
-
-
-    document.querySelector('#btn-success').addEventListener(NativeEvents.Click, (e) =>
-    {
-        MessageBoxUtilities.showSuccess({
-            message: 'good job',
-            title: 'fuck u nice',
-        });
-    });
-
-
-    document.querySelector('#btn-confirm').addEventListener(NativeEvents.Click, (e) =>
-    {
-        const confirmMessage = new MessageBoxConfirm('Are you sure?');
-
-        confirmMessage.confirm({
-            onSuccess: () =>
-            {
-                alert('yes');
-            },
-        });
-
-    });
-
-
-}
 
