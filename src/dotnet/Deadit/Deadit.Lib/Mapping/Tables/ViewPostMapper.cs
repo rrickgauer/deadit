e140f6d5936a9bb2da95f5492984f132850fd6e1
Ryan Rickgauer <rrickgauer1@gmail.com>
2024-05-22 15:00:32 -0500

    add 3 new fields to post

--- src/dotnet/Deadit/Deadit.Lib/Mapping/Tables/ViewPostMapper.cs
+++ src/dotnet/Deadit/Deadit.Lib/Mapping/Tables/ViewPostMapper.cs
@@ -11,12 +11,15 @@ public class ViewPostMapper : TableMapper<ViewPost>
     {
         ViewPost result = InheritanceUtility.GetParentProperties<ViewCommunity, ViewPost, ViewCommunityTableMapper>(row);
 
-        result.PostId          = row.Field<Guid?>(GetColumnName(nameof(result.PostId)));
-        result.PostCommunityId = row.Field<uint?>(GetColumnName(nameof(result.PostCommunityId)));
-        result.PostTitle       = row.Field<string?>(GetColumnName(nameof(result.PostTitle)));
-        result.PostType        = (PostType?)row.Field<ushort?>(GetColumnName(nameof(result.PostType)));
-        result.PostAuthorId    = row.Field<uint?>(GetColumnName(nameof(result.PostAuthorId)));
-        result.PostCreatedOn   = row.Field<DateTime?>(GetColumnName(nameof(result.PostCreatedOn)));
+        result.PostId            = row.Field<Guid?>(GetColumnName(nameof(result.PostId)));
+        result.PostCommunityId   = row.Field<uint?>(GetColumnName(nameof(result.PostCommunityId)));
+        result.PostTitle         = row.Field<string?>(GetColumnName(nameof(result.PostTitle)));
+        result.PostType          = (PostType?)row.Field<ushort?>(GetColumnName(nameof(result.PostType)));
+        result.PostAuthorId      = row.Field<uint?>(GetColumnName(nameof(result.PostAuthorId)));
+        result.PostCreatedOn     = row.Field<DateTime?>(GetColumnName(nameof(result.PostCreatedOn)));
+        result.PostDeletedOn     = row.Field<DateTime?>(GetColumnName(nameof(result.PostDeletedOn)));
+        result.PostArchivedOn    = row.Field<DateTime?>(GetColumnName(nameof(result.PostArchivedOn)));
+        result.PostModRemovedOn  = row.Field<DateTime?>(GetColumnName(nameof(result.PostModRemovedOn)));
         result.PostCountComments = Convert.ToUInt32(row.Field<object?>(GetColumnName(nameof(result.PostCountComments))));
 
         row.SetVotingValues(result);
