9420aa8d43e36616f074f7a8ec28bbd8c0c25380
Ryan Rickgauer <rrickgauer1@gmail.com>
2024-05-24 10:08:20 -0500

    explicitly state static files folder (#56)

--- src/dotnet/Deadit/Deadit.Lib/Domain/Configurations/ConfigurationDev.cs
+++ src/dotnet/Deadit/Deadit.Lib/Domain/Configurations/ConfigurationDev.cs
@@ -5,4 +5,7 @@ public class ConfigurationDev : ConfigurationProduction, IConfigs
     public override bool IsProduction => false;
 
     public override string DbDataBase => "Deadit_Dev";
+
+    public override string StaticWebFilesPath => @"C:\xampp\htdocs\files\deadit\src\dotnet\Deadit\Deadit.WebGui\wwwroot";
 }
+
